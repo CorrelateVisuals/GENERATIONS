18b9cb112e1d391e986b006981bd8bff1dfc850e
CorrelateVisuals <jakobpovel@protonmail.com>
2024-09-03 18:21:58 +0200

    ++

--- src/BaseClasses.h
+++ src/BaseClasses.h
@@ -105,46 +105,6 @@ class Device {
       const VkPhysicalDevice& physical) const;
 };
 
-class DescriptorInterface {
- public:
-  size_t writeIndex{};
-  std::array<VkDescriptorSet, MAX_FRAMES_IN_FLIGHT> sets{};
-  VkDescriptorSetLayout setLayout{};
-  std::array<VkDescriptorSetLayoutBinding, NUM_DESCRIPTORS> setLayoutBindings{};
-  std::array<std::array<VkWriteDescriptorSet, NUM_DESCRIPTORS>,
-             MAX_FRAMES_IN_FLIGHT>
-      descriptorWrites{};
-  std::vector<VkDescriptorPoolSize> poolSizes{};
-
-  DescriptorInterface() = default;
-  virtual ~DescriptorInterface();
-
-  void initialzeSets();
-  void updateSets();
-
- private:
-  VkDescriptorPool pool;
-
-  void createSetLayout();
-  void createPool();
-  void allocateSets();
-};
-
-class Descriptor {
- public:
-  Descriptor() = default;
-  virtual ~Descriptor(){};
-
- protected:
-  size_t myIndex{0};
-  VkDescriptorPoolSize poolSize{};
-  VkDescriptorSetLayoutBinding setLayoutBinding{};
-  struct DescriptorInformation {
-    std::variant<VkDescriptorBufferInfo, VkDescriptorImageInfo> previousFrame{};
-    std::variant<VkDescriptorBufferInfo, VkDescriptorImageInfo> currentFrame{};
-  } info;
-};
-
 class CommandBuffers {
  public:
   VkCommandPool pool{};
@@ -334,6 +294,45 @@ class Swapchain {
 };
 
 // Resources
+class DescriptorInterface {
+public:
+    size_t writeIndex{};
+    std::array<VkDescriptorSet, MAX_FRAMES_IN_FLIGHT> sets{};
+    VkDescriptorSetLayout setLayout{};
+    std::array<VkDescriptorSetLayoutBinding, NUM_DESCRIPTORS> setLayoutBindings{};
+    std::array<std::array<VkWriteDescriptorSet, NUM_DESCRIPTORS>,
+        MAX_FRAMES_IN_FLIGHT>
+        descriptorWrites{};
+    std::vector<VkDescriptorPoolSize> poolSizes{};
+
+    DescriptorInterface() = default;
+    virtual ~DescriptorInterface();
+
+    void initialzeSets();
+    void updateSets();
+
+private:
+    VkDescriptorPool pool;
+
+    void createSetLayout();
+    void createPool();
+    void allocateSets();
+};
+
+class Descriptor {
+public:
+    Descriptor() = default;
+    virtual ~Descriptor() {};
+
+protected:
+    size_t myIndex{ 0 };
+    VkDescriptorPoolSize poolSize{};
+    VkDescriptorSetLayoutBinding setLayoutBinding{};
+    struct DescriptorInformation {
+        std::variant<VkDescriptorBufferInfo, VkDescriptorImageInfo> previousFrame{};
+        std::variant<VkDescriptorBufferInfo, VkDescriptorImageInfo> currentFrame{};
+    } info;
+};
 
 struct PushConstants {
   VkShaderStageFlags shaderStage{};
